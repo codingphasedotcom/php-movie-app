cfcd4a607981d75f5fb65d6ce2965a9c2dc2c358
CodingPhase <joesantosgarcia@gmail.com>
2022-12-18 20:48:48 -0500

    connect to movie api and display movie results

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
-Route::get('/search', function () {
-    return view('pages.results');
+Route::get('/search', function (Request $request) {
+    // return $request->input('query');
+    $query = $request->input('query');
+    $request_url = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=" . env('WATCHMODE_KEY') . "&search_field=name&search_value=". $query;
+
+    $response = Http::get($request_url);
+    $results = $response->json();
+    $results = $results['results'];
+    return view('pages.results', [
+        "results" => $results, 
+        "query" => ucwords($query)
+    ]);
 });
 Route::get('/movie', function () {
     return view('pages.single');
